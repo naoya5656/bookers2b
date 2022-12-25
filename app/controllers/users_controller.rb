759e06b26e81ac017558950a1f3c48b8ed671dd4
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2022-12-25 03:46:57 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+   before_action :is_matching_login_user, only: [:edit, :update]
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(@user)
+    else
+    render :edit
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+   def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end
+   end
+end
